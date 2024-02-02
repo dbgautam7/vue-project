27607c7a2904c3471596c227c129670e8593755a
DB Gautam <db7@DBs-MacBook-Air.local>
2024-02-03 00:12:52 +0545

    setup:project setup with typescript and tailwind

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import HomeIcon from './icons/IconHome.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <HomeIcon />
+    </template>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+  </WelcomeItem>
+</template>
---
+++ src/components/WelcomeItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mt-8 flex relative">
+    <i class="flex place-items-center place-content-center w-[50px] h-[50px] border rounded-md">
+      <slot name="icon"></slot>
+    </i>
+    <div class="flex-1 ml-12">
+      <h3 class="text-xl">
+        <slot name="heading"></slot>
+      </h3>
+      <slot></slot>
+    </div>
+  </div>
+</template>
